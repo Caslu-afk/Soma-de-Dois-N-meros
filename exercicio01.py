d660886922c4a1c8bcfb75c422f4fc6a360b2781
Lucas <azevedolucasedu@gmail.com>
2026-01-20 21:03:28 -0300

    Add files via upload
    Primeiro exercicio de logica de programção

---
+++ exercicio01.py
@@ -0,0 +1,8 @@
+#Soma de Dois Números 
+#Descrição: Escreva um programa que leia dois números inteiros fornecidos pelo usuário e exiba a soma deles.
+
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo numero: "))
+soma = n1 + n2
+
+print(soma)
